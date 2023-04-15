fde3b8d48c499bc86bb2956a1c2e69e57e2cd741
alexfarias1974 <alex_wfariaas@yahoo.com.br>
2023-04-15 10:18:17 -0400

    feat: newProduct array  to carousel

--- src/db/database.jsx
+++ src/db/database.jsx
@@ -11,6 +11,14 @@ import Packs from "../assets/class_product_packs.png";
 import Sleep from "../assets/class_product_sleephealth.png";
 import Turbo from "../assets/class_product_turboline.png";
 import Gourmet from "../assets/class_product_gourmetline.png";
+import NewBar from "../assets/new_product_bar.png";
+import NewTestopack from "../assets/new_product_testopack.png";
+import NewFatburner from "../assets/new_product_fatburner.png";
+import NewColostrum from "../assets/new_product_colostrum.png";
+import NewIsolatehd from "../assets/new_product_isolatehd.png";
+import NewBonecrusher from "../assets/new_product_bonecrusher.png";
+import NewPreworkout from "../assets/new_product_preworkout.png";
+import New9eaa from "../assets/new_product_9eaa.png";
 
 
 export const classProduct = [
@@ -78,5 +86,73 @@ export const classProduct = [
         id: 13,
         img: Gourmet,
         name: "LINHA GOURMET",
+    },
+]
+
+export const newProducts = [
+    {
+        id: 1,
+        img: NewBar,
+        name: "Barra de proteína",
+        description: "BARRA DE PROTEÍNA GOURMET C/COLOSTRO - DISPLAY C/ 12",
+        price: "R$ 129,90",
+        paymentConditions: "Em até 6 x R$ 21,65 sem juros",
+    },
+    {
+        id: 2,
+        img: NewTestopack,
+        name: "Testopack",
+        description: "MONSTROUS TESTOPACK 30 PACKS BLACK SKULL",
+        price: "R$ 199,90",
+        paymentConditions: "Em até 8 x R$ 24,98 sem juros",
+    },
+    {
+        id: 3,
+        img: NewFatburner,
+        name: "Fat burner",
+        description: "FAT BURNER",
+        price: "R$ 99,90",
+        paymentConditions: "Em até 4 x R$ 24,97 sem juros",
+    },
+    {
+        id: 4,
+        img: NewColostrum,
+        name: "Colostro",
+        description: "COLOSTRUM (COLOSTRO) BLACK SKULL - 60 TABS",
+        price: "R$ 159,90",
+        paymentConditions: "Em até 7 x R$ 22,84 sem juros",
+    },
+    {
+        id: 5,
+        img: NewIsolatehd,
+        name: "Proteína concentrada",
+        description: "ISOLATE HD 900G",
+        price: "R$ 259,90",
+        paymentConditions: "Em até 8 x R$ 32,48 sem juros",
+    },
+    {
+        id: 6,
+        img: NewBonecrusher,
+        name: "Pré-treino Bone Crusher",
+        description: "BONE CRUSHER GAME ON 300G",
+        price: "R$ 109,90",
+        paymentConditions: "Em até 5 x R$ 21,98 sem juros",
+    },
+    {
+        id: 7,
+        img: NewPreworkout,
+        name: "Pré-treino Legacy",
+        description: "PRÉ-TREINO LEGACY - 300G",
+        price: "R$ 109,90",
+        paymentConditions: "Em até 5 x R$ 21,98 sem juros",
+    },
+    {
+        id: 8,
+        img: New9eaa,
+        name: "Composto de Aminoácidos 9EAA",
+        description: "9EAA - 300G",
+        price: "R$ 89,90",
+        paymentConditions: "Em até 4 x R$ 22,47 sem juros",
+
     },
 ]
