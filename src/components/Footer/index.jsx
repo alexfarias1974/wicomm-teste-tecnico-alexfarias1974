6f3ebf764d8b5fb8b9280655b5471b4e92cf03fb
alexfarias1974 <alex_wfariaas@yahoo.com.br>
2023-04-17 11:38:09 -0400

    fix: footer registered trademark

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,9 +3,9 @@ import VtexLogo from "../../assets/vtex_white_logo.png";
 
 const Footer = () => {
     return (
-        <div className="h-[88px] bg-black1 flex justify-between px-14">
+        <div className="h-[88px] bg-black1 flex justify-between px-14 border-t-[1px] border-t-solid border-t-dark3">
             <div className="flex items-center">
-                <p className="font-barlow font-normal text-base text-white">Black Skull 2022. Todos os direitos reservados &#169;.</p>
+                <p className="font-barlow font-normal text-base text-white">Black Skull 2022. Todos os direitos reservados <span className="text-2xl">&#174;</span>.</p>
             </div>
             <div className="flex gap-[40px] items-center">
                 <img src={WicommLogo} alt="" className="w-[96px] h-[24px]"/>
