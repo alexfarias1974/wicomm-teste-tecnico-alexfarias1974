2883c24d0702f52fc000a8c328bc67571013dc21
alexfarias1974 <alex_wfariaas@yahoo.com.br>
2023-04-17 17:12:07 -0400

    fix: navbar hover

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -7,21 +7,35 @@ import {BsCart3} from "react-icons/bs";
 import "./style.css";
 
 const NavBar = () => {
-    
-
     return (
         <div className="flex flex-row bg-black1 font-barlow h-[72px] items-center justify-center">
-            <picture>
+            <div>
                 <img src={LogoBSNavBar} alt="Logo BlackSkull"/>
-            </picture>
+            </div>
             <nav className="font-bold text-[14px] text-gray1 mx-5">
                 <ul className="flex gap-7 justify-center">
-                    <li className="hover:text-white cursor-pointer hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">CATEGORIAS</li>
-                    <li className="hover:text-white cursor-pointer hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">VESTUÁRIO</li>
-                    <li className="hover:text-white cursor-pointer hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">OBJETIVOS</li>
-                    <li className="hover:text-white cursor-pointer hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">PROMOÇÕES</li>
-                    <li className="hover:text-white cursor-pointer hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">ATLETAS</li>
-                    <li className="hover:text-white cursor-pointer hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">ASSINATURA</li>
+                    <div className="hover:text-white hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">
+                        <li className="cursor-pointer mt-7">CATEGORIAS</li>
+                    </div>
+                    <div className="hover:text-white hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">
+                        <li className="cursor-pointer mt-7">VESTUÁRIO</li>
+                    </div>
+                    <div className="hover:text-white hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">
+
+                        <li className="cursor-pointer mt-7">OBJETIVOS</li>
+                    </div>
+                    <div className="hover:text-white hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">
+
+                        <li className="cursor-pointer mt-7">PROMOÇÕES</li>
+                    </div>
+                    <div className="hover:text-white hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">
+
+                        <li className="cursor-pointer mt-7">ATLETAS</li>
+                    </div>
+                    <div className="hover:text-white hover:bg-dark2 hover:border-orange1 hover:border-b-[3px] h-[72px] mt-3 transition-all">
+
+                        <li className="cursor-pointer mt-7">ASSINATURA</li>
+                    </div>
                 </ul>
             </nav>
             <div className="flex items-center mr-4">
@@ -29,12 +43,10 @@ const NavBar = () => {
                     type="text"
                     placeholder="Buscar"
                     className="h-10 w-[216px] font-medium text-xs text-gray1 bg-dark1 border border-solid border-dark3 rounded"
-                    
                 />
                 <span className="absolute z-10 ml-[190px] text-gray1 text-xl focus:text-dark1">
                     <FiSearch />
                 </span>
-
             </div>
             <div className="flex gap-3 text-2xl">
                 <span className="text-gray1 cursor-pointer">
