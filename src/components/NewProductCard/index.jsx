47ea1d7a82fed88b7c3b7815575c897c81d0ac4e
alexfarias1974 <alex_wfariaas@yahoo.com.br>
2023-04-17 23:09:11 -0400

    feat: hover button buy

--- src/components/NewProductCard/index.jsx
+++ src/components/NewProductCard/index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-
+import {AiOutlineStar} from "react-icons/ai";
+import {BsCart3} from "react-icons/bs";
 
 const NewProductCard = ({newImg, newName, description, price, paymentConditions}) => {
     return (
-        <>        
-            <div className="w-[277px] h-[341px] border-2 border-background rounded">
+        <div className="card-product flex flex-col">        
+            <div className="bg-white w-[277px] h-[341px] border-2 border-background rounded">
+                <div className="absolute flex flex-col ml-[235px] mt-[18px]">
+                    <span className="text-2xl text-gray1 cursor-pointer hover:text-orange1 mb-2">
+                        <AiOutlineStar />
+                    </span>
+                    <span className="text-2xl text-gray1 cursor-pointer hover:text-orange1">
+                        <BsCart3 />
+                    </span>
+                </div>
                 <div className="flex justify-center mt-8 mb-6">
                     <picture>
                         <img className="w-40" src={newImg} alt={newName} />
@@ -24,7 +33,12 @@ const NewProductCard = ({newImg, newName, description, price, paymentConditions}
                     </h4>
                 </div>
             </div>
-        </>
+                <div className="move-button-buy w-[277px] h-[80px]">
+                    <button className="w-[273px] h-[60px] font-barlow font-bold text-xl bg-orange1 rounded hover:bg-orange2">
+                        COMPRAR
+                    </button>
+            </div>
+        </div>
     )
 }
 
